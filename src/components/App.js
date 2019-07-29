5a80dd76551c396d0c0ef58b46549ef223002630
Jake Tripp <shmogens@gmail.com>
2019-07-28 23:36:57 -0500

    Remove react favicon, work on some styling

--- src/components/App.js
+++ src/components/App.js
@@ -53,8 +53,6 @@ class App extends Component {
 
     const verticalStepsArr = Array.from({ length: verticalSteps }, (x, i) => i);
 
-    // addRows(verticalSteps, SELECTOR);
-
     // iterate vertically and make new array of rows
     const colorTable = leftColorArr.map((leftCell, i) => {
       const rightCell = rightColorArr[i];
@@ -63,71 +61,73 @@ class App extends Component {
 
     // updateUI(colorTable, SELECTOR, blockWidth, blockHeight);
 
-    // function addRows(verticalSteps, selector) {
-    //   for (let i = 0; i < verticalSteps; i++) {
-    //     const div = document.createElement("div");
-    //     div.classList.add(`row-${i}`);
-    //     document.querySelector(selector).appendChild(div)
-    //   }
-    // }
-
-    // function updateUI(colorTable, selector, blockWidth, blockHeight) {
-    //   colorTable.forEach((row, i) => {
-    //     row.forEach(color => {
-    //       const div = document.createElement("div");
-    //       const colorStr = toRGBString(color);
-    //       div.classList.add('block');
-    //       div.innerText = colorStr;
-    //       div.style.background = colorStr
-    //       div.style.height = blockHeight;
-    //       div.style.width = blockWidth;
-    //       document.querySelector(`.row-${i}`).appendChild(div);
-    //     });
-    //   });
-    // }
-
     return (
       <div className="App">
-        <div className="form-input">
-          <input
-            type="color"
-            name="topLeft"
-            title="Top left color"
-            value={this.state.topLeft}
-            onChange={this.handleColorChange}
-          />
-        </div>
+        <div className="form">
+          <div className="form-colors">
+            <div className="form-input">
+              <input
+                type="color"
+                name="topLeft"
+                title="Top left color"
+                value={this.state.topLeft}
+                onChange={this.handleColorChange}
+              />
+            </div>
 
-        <div className="form-input">
-          <input
-            type="color"
-            name="topRight"
-            title="Top right color"
-            value={this.state.topRight}
-            onChange={this.handleColorChange}
-          />
-        </div>
+            <div className="form-input">
+              <input
+                type="color"
+                name="topRight"
+                title="Top right color"
+                value={this.state.topRight}
+                onChange={this.handleColorChange}
+              />
+            </div>
 
-        <div className="form-input">
-          <input
-            type="color"
-            name="bottomLeft"
-            title="Bottom left color"
-            value={this.state.bottomLeft}
-            onChange={this.handleColorChange}
-          />
-        </div>
+            <div className="form-input">
+              <input
+                type="color"
+                name="bottomLeft"
+                title="Bottom left color"
+                value={this.state.bottomLeft}
+                onChange={this.handleColorChange}
+              />
+            </div>
 
-        <div className="form-input">
-          <input
-            type="color"
-            name="bottomRight"
-            title="Bottom right color"
-            value={this.state.bottomRight}
-            onChange={this.handleColorChange}
-          />
+            <div className="form-input">
+              <input
+                type="color"
+                name="bottomRight"
+                title="Bottom right color"
+                value={this.state.bottomRight}
+                onChange={this.handleColorChange}
+              />
+            </div>
+          </div>
+          <div className="form-steps">
+            <label htmlFor="verticalSteps">Vertical Steps: {this.state.verticalSteps}</label>
+            <input
+              type="range"
+              name="verticalSteps"
+              min="2"
+              max="20"
+              step="1"
+              value={this.state.verticalSteps}
+              onChange={this.handleColorChange}
+            />
+            <label htmlFor="horizontalSteps">Horizontal Steps: {this.state.horizontalSteps}</label>
+            <input
+              type="range"
+              name="horizontalSteps"
+              min="2"
+              max="20"
+              step="1"
+              value={this.state.horizontalSteps}
+              onChange={this.handleColorChange}
+            />
+          </div>
         </div>
-
         <Grid
           table={colorTable}
           verticalSteps={this.state.verticalSteps}
