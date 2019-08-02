b3b14ca5027d5c723626e3118f1c7d9e8e56ad60
Jake <shmogens@gmail.com>
2019-08-02 00:53:44 -0500

    Updates

---
+++ precache-manifest.e723c3f4badda52b98a551515da3d7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc5a0aac8539e70e4e311f043cccae1",
+    "url": "/color-grid/index.html"
+  },
+  {
+    "revision": "4e5f1e903d5eb2e7e2c9",
+    "url": "/color-grid/static/css/main.84e99af2.chunk.css"
+  },
+  {
+    "revision": "3b95742fa1c76ba0bbcb",
+    "url": "/color-grid/static/js/2.1c5b36f4.chunk.js"
+  },
+  {
+    "revision": "4e5f1e903d5eb2e7e2c9",
+    "url": "/color-grid/static/js/main.f12b04d5.chunk.js"
+  },
+  {
+    "revision": "0644a659c14c873f5731",
+    "url": "/color-grid/static/js/runtime~main.b4e9e7f9.js"
+  },
+  {
+    "revision": "8f0bc01ce5e5becef482d277cb72b728",
+    "url": "/color-grid/static/media/ProximaNova-Thin.8f0bc01c.otf"
+  }
+]);
